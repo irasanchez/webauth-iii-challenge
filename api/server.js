6b1dbd15137c54a6f255d05efae7b80112dab5ba
iradsanchez <ira.d.sanchez@gmail.com>
2019-04-16 21:03:20 -0700

    Add Organized Server.js File

--- api/server.js
+++ api/server.js
@@ -1,19 +1,22 @@
+//middleware imports
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 
-//import routers here
-// const authRouter = require('../auth/auth-router.js');
-// const usersRouter = require('../users/users-router.js');
+//router imports
+const authRouter = require("../auth/auth-router.js");
+const usersRouter = require("../users/users-router.js");
 
+//server is made
 const server = express();
 
+//middleware and server used
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
-//use routers here
-// server.use('/api/auth', authRouter);
-// server.use('/api/users', usersRouter);
+//routers used
+server.use("/api/auth", authRouter);
+server.use("/api/users", usersRouter);
 
 module.exports = server;
