6d993c3925d8e46820ab30ca2da88d5e2fc13047
iradsanchez <ira.d.sanchez@gmail.com>
2019-04-16 21:49:11 -0700

    Add GET and Restricted Middleware

---
+++ auth/restricted-middleware.js
@@ -0,0 +1,17 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (token) {
+    jwt.verify(token, "this is the secret", (error, decodedToken) => {
+      if (error) {
+        res.status(401).json({ message: "Invalid credentials" });
+      } else {
+        req.decodedToken = decodedToken;
+        next();
+      }
+    });
+  } else {
+    res.status(400).json({ message: "No credentials provided" });
+};
--- users/users-router.js
+++ users/users-router.js
@@ -4,6 +4,14 @@ const jwt = require("jsonwebtoken");
 const Users = require("./users-model");
 const restricted = require("../auth/restricted-middleware");
 
-// | GET    | /api/users    | If the user is logged in, respond with an array of all the users contained in the database. If the user is not logged in respond with the correct status code and the message: 'You shall not pass!'. Use this endpoint to verify that the password is hashed before it is saved. |
-
-// router.get();
+// GET /api/users
+//If the user is logged in => add restricted middleware
+// Use this endpoint to verify that the password is hashed before it is saved.
+router.get("/users", restricted, (req, res) => {
+  // respond with an array of all the users contained in the database.
+  Users.find()
+    .then(users => {
+      res.json(users);
+    })
+    .catch(error => res.send(error));
+});
