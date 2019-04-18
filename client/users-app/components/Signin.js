2511a568b48cc98ee625b5ab9a9ec66a97c3ca2a
iradsanchez <ira.d.sanchez@gmail.com>
2019-04-17 23:14:55 -0700

    Add Client File Structure

---
+++ client/users-app/components/Signin.js
@@ -0,0 +1,1 @@
+// - the `/signin` route should provide a form to gather `username` and `password` for the user and make a `POST` request to the `/api/login` route on the API. Upon successful login, persist the returned token to the browser's local storage and redirect the user to the `/users` route.
---
+++ client/users-app/components/UserList.js
@@ -0,0 +1,2 @@
+// - the `/users` route should read the token from local storage and make a `GET` request to the `/api/users` route on the API attaching the token as the value of the `Authorization` header.
+// - provide a button to `sign out` that will remove the token from local storage.
